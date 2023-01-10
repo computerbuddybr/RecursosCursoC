ea3cf48d5431ad1959ce6db619f620d5fb0398d3
Adriana Cerdeira - Computer Buddy BR <adriana.cerdeira@gmail.com>
2023-01-10 17:09:47 -0300

    Depois de acabar a gravação

--- Parte08-InputOutput/terminal.c
+++ Parte08-InputOutput/terminal.c
@@ -5,19 +5,24 @@ void main(){
     int age;
     char name[20];
 
-   /*
+
    printf("Digite seu nome: ");
    //No scanf, o primeiro argumento é um string com o tipo de dado esperado e o segundo é o endereço da variável onde será guardado o dado. Isso porque o scanf espera um ponteiro
     scanf("%s", name);
-
+    fflush(stdin);
     printf("Digite sua idade: ");
     scanf("%d", &age);
 
+    printf("Digite seu nome e idade: ");
+    scanf("%s%d", name, &age);
+    fflush(stdin);
     printf("Seu nome é %s e sua idade é %d\n", name, age);
-*/ 
+
     int numbers[4];
-    //printf("Insira 4 números:");
-    //scanf("%d", numbers);
+    // printf("Insira 4 números:");
+    // scanf("%d", numbers);
+    // scanf("%d%d%d%d", numbers);
+    //scanf("%d%d%d%d", numbers, (numbers + 1), (numbers + 2), (numbers + 3));
     //Posso fazer um loop pra pessoa colocar em uma linha diferente
     for(int i = 0; i < 4; i++){
         printf("Insira um número:");
@@ -29,6 +34,9 @@ void main(){
     //Ou posso fazer uma vez só já avisando o scanf que ele vai receber mais de um dado separado por espaços
     printf("Insira um número:");
     scanf("%d%d%d%d", &numbers[0],&numbers[1],&numbers[2], &numbers[3]);
+     for(int i = 0; i < 4; i++){
+        printf("%d\n", numbers[i]);
+    }
 
    
 
